0e60ddaad2ebb49895e25abaee378eee363900f7
Meghal1879 <87679139+Meghal1879@users.noreply.github.com>
2022-05-12 14:45:59 -0400

    new msg

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HomeComponent() {
   return (
       <div>
-          <h3> i am home component</h3>
+          <h3> i am home and only home component</h3>
           </div>
 
   )
